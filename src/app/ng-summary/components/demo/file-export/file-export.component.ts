9116ac7926809d4514888f6c9a6b6e4fd666d92d
qinzhihao-conghua <1043419697@qq.com>
2020-10-31 15:45:13 +0800

    saveAs有效，需要在文件名后面添加好对应的后缀才能正确打开文件，docx类型不支持，无法打开文件

--- src/app/ng-summary/components/demo/file-export/file-export.component.ts
+++ src/app/ng-summary/components/demo/file-export/file-export.component.ts
@@ -16,13 +16,14 @@ export class FileExportComponent implements OnInit {
 
   export() {
     const dom = document.getElementById('downloadContent').innerHTML;
+    // docx类型 Word无法打开。提示有无法读取的内容
     const word = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document;charset=utf-8';
     const word1 = 'application/msword;charset=utf-8';
     const blob = new Blob(['\ufeff' + dom], { type: word1 });
-    // saveAs(blob, '成绩单');
+    saveAs(blob, '成绩单.doc');
 
-    let url = URL.createObjectURL(blob);
-    (document.getElementById('download') as any).href = url;
+    // let url = URL.createObjectURL(blob);
+    // (document.getElementById('download') as any).href = url;
   }
 
 }
