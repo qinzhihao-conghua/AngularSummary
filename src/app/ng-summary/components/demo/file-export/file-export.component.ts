349f34c8fcf7bba97671a02435feff4639e51420
qinzhihao-conghua <1043419697@qq.com>
2020-10-31 15:39:02 +0800

    原生实现导出 saveAs无效

--- src/app/ng-summary/components/demo/file-export/file-export.component.ts
+++ src/app/ng-summary/components/demo/file-export/file-export.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'zh-file-export',
@@ -8,8 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class FileExportComponent implements OnInit {
 
   constructor() { }
+  @ViewChild('downloadContent') downloadContent: ElementRef;
 
   ngOnInit(): void {
+  }
+
+  export() {
+    const dom = document.getElementById('downloadContent').innerHTML;
+    const word = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document;charset=utf-8';
+    const word1 = 'application/msword;charset=utf-8';
+    const blob = new Blob(['\ufeff' + dom], { type: word1 });
+    // saveAs(blob, '成绩单');
+
+    let url = URL.createObjectURL(blob);
+    (document.getElementById('download') as any).href = url;
   }
 
 }
